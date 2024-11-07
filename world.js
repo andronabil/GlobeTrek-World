054e77a9419165b5982653200dc349760aea226a
andronabil <andonabil5@gmail.com>
2024-11-07 14:38:20 +0200

    update

---
+++ world.js
@@ -0,0 +1,24 @@
+let btnCard = document.querySelectorAll(".btn");
+let bigContainer=document.getElementById("bigContainer")
+let  paymentNow= document.getElementById("paymentNow")
+let exitButton=document.getElementById("exitButton")
+let successMade=document.getElementById("successMade")
+console.log(successMade)
+
+
+btnCard.forEach(eo => {
+    eo.addEventListener("click", () => {
+        bigContainer.classList.remove("display-none")    
+    });
+});
+exitButton.addEventListener("click",()=>{
+    bigContainer.classList.add("display-none")  
+})
+paymentNow.addEventListener("click",()=>{
+    setTimeout((eo) => {
+        bigContainer.classList.add("display-none")
+    }, 3000);
+})
+
+
+
